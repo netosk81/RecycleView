684573c19ea9f252cf75a29f9973d7e6c885bdc5
netosk81 <hk.neto@gmail.com>
2023-03-22 20:08:32 -0300

    Initial commit

---
+++ app/src/main/java/com/example/recycleview/activity/activity/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.recycleview.activity.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.example.recycleview.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        //Configurar adapter
+
+
+        //Configurar RecyclerView
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true); //otimizador para recyclerView(colocando um tamanho fixo)
+        //recyclerView.setAdapter();
+    }
+
+
+}
---
+++ app/src/main/java/com/example/recycleview/activity/adapter/Adapter.java
@@ -0,0 +1,42 @@
+package com.example.recycleview.activity.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.w3c.dom.Text;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
+
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView titulo;
+        TextView ano;
+        TextView genero;
+
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+}
